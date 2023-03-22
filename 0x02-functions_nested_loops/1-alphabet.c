02486cb32cf28fad1913ddfbee60f270c3f53866
Mouad Sodor <sodormouad@gmail.com>
2023-03-22 00:59:09 +0000

    no fibo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - func
+*
+* Return - 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int b = 0
+
+	while (b < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(a);
+			c++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
