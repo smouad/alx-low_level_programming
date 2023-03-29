71db4ce40a07b0db2e738d6cfdfae392fe0de16d
Mouad Sodor <sodormouad@gmail.com>
2023-03-29 19:58:41 +0000

    first

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an array of int
+ * @a: array to reverse
+ * @n: size of array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, i;
+
+	i = 0;
+	while (i < n / 2)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+		i++;
+		n--;
+	}
+}
