5dd01d655562153d174c4c3b9a88bafdb9adc603
Mouad Sodor <sodormouad@gmail.com>
2023-03-30 18:39:06 +0000

    bonus

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,17 @@
 
 void print_number(int n)
 {
+	int x;
+
 	if (n < 0)
 	{
 		n *= -1;
 		_putchar('-');
 	}
-	if (n / 10)
+	x = n;
+	if (x / 10)
 	{
-		print_number(n / 10);
+		print_number(x / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((x % 10) + '0');
 }
