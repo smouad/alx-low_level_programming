9d613dd468475145bf9fd02abe9a5b7413f6cca7
Mouad Sodor <sodormouad@gmail.com>
2023-03-30 18:41:53 +0000

    bonus

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,52 @@
 #include "main.h"
+
 /**
- * print_number - prints the number given on the sdout
- * @n: the number to print
+ * base10 - power in 10 base
+ * @n: an exponent
+ * Return: returns 10 to power exponent
+ */
+int base10(int n)
+{
+	int base = 10;
+
+	while (n > 0)
+	{
+		base *= 10;
+		n--;
+	}
+	return (base);
+}
+
+/**
+ * print_number - prints integers enters as parameters using putchar
+ * @n: integer to print
  * Return: void
  */
-
 void print_number(int n)
 {
-	int x;
+	int power;
+
+	power = base10(8);
 
 	if (n < 0)
 	{
-		n *= -1;
 		_putchar('-');
+		n *= -1;
 	}
-	x = n;
-	if (x / 10)
+
+	if (n == 0)
+		_putchar('0');
+
+	else
 	{
-		print_number(x / 10);
+		while (n / power == 0)
+			power /= 10;
+
+		while (power >= 1)
+		{
+			_putchar((n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
 	}
-	_putchar((x % 10) + '0');
 }
