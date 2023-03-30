669bedc5aa72657c5a0d1e01f5f846fdec082d11
Mouad Sodor <sodormouad@gmail.com>
2023-03-30 18:26:13 +0000

    bonus

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - prints the number given on the sdout
+ * @n: the number to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
