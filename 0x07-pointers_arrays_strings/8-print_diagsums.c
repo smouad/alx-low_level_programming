b1836ab32baadf0a9e001cff1ab80aa0a5b32e85
Mouad Sodor <sodormouad@gmail.com>
2023-04-04 00:30:10 +0000

    all

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_diagsums - prints the sums of the two diagonals of a square matrix
+ * @a: pointer to start of matrix
+ * @size: width of matrix column
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+    int i = 0;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <string.h>
+#include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
