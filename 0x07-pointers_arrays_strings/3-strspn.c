55cfafb0faa1b8fa90db080aa90efd7fa102e34d
Mouad Sodor <sodormouad@gmail.com>
2023-04-04 00:12:12 +0000

    mdt

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,24 +12,24 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int count = 0, i, j;
+	int count = 0, i, j;
 
-    i = 0;
-    while (s[i] != '\0')
-    {
-        if (s[i] != 32)
-        {
-            j = 0;
-            while (accept[j] != '\0')
-            {
-                if (s[i] == accept[j])
-                    count++;
-                j++;
-            }
-        }
-        else
-            return (count);
-        i++;
-    }
-    return (count);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != 32)
+		{
+			j = 0;
+			while (accept[j] != '\0')
+			{
+				if (s[i] == accept[j])
+					count++;
+				j++;
+			}
+		}
+		else
+			return (count);
+		i++;
+	}
+	return (count);
 }
