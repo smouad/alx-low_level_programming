a8934b1465ee0822636b7dbc433ce95a0c610836
Mouad Sodor <sodormouad@gmail.com>
2023-04-06 00:39:58 +0000

    mandatory

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+/**
+ * _pow_recursion - calculate with recursion @x to the power of @y
+ * @x: first int
+ * @y: second int
+ * Return: the result and -1 if @y less tha 0 
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+	return (1);
+}
