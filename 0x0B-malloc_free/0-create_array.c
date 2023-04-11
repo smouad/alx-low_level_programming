d006c9e1ef6ee0ee5b74271bcdd76a3f2afe2dad
Mouad Sodor <sodormouad@gmail.com>
2023-04-11 11:45:41 +0000

    mandatory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * create_array - create an aray of size @size and fill it with @c
+ * @size: size
+ * @c: character to fill with
+ * Return: a pointer to the new string
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	if (!size)
+		return (NULL);
+	str = malloc(sizeof(char) * size + 1);
+	if (!str)
+		return (NULL);
+	memset(str, c, size);
+	return (str);
+}
