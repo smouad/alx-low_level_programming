ee2693372c9c519e8f97a1f2355bfce0fbce4e70
Mouad Sodor <sodormouad@gmail.com>
2023-04-11 22:11:23 +0000

    mandatory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 = "";i
+		s2 = "";
 	for (len_s1 = 0; s1[len_s1]; len_s1++)
 		;
 	for (len_s2 = 0; s2[len_s2]; len_s2++)
 		;
-	str = malloc(sizeof(char) * (len_s1 + len_s2 + 1))
+	str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 	if (!str)
 		return (NULL);
 	for (i = 0; i < len_s1; i++)
