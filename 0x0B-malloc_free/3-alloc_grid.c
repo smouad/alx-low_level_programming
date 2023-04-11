eeb962667e9106e27bc3dccf1d3b6a5c7ebb7fc0
Mouad Sodor <sodormouad@gmail.com>
2023-04-11 22:48:33 +0000

    mandatory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,25 +9,27 @@
 
 int **alloc_grid(int width, int height)
 {
-    int i, j;
-    int **grid;
+	int i, j;
+	int **grid;
 
-    grid = malloc(sizeof(int *) * height);
-    if (!grid)
-        return (NULL);
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = malloc(sizeof(int) * width);
-        if (!grid[i])
-        {
-            for (j = 0; j <= i; j++)
-                free(grid[j]);
-            free(grid);
-        }
-        for (j = 0; j < width; j++)
-        {
-            grid[i][j] = 0;
-        }
-    }
-    return (grid);
+	if (!width || !height)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (!grid)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (!grid[i])
+		{
+			for (j = 0; j <= i; j++)
+				free(grid[j]);
+			free(grid);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
 }
