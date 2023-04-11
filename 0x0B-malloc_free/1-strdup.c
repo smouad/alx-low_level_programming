cd408026c57908aac2e2fcf2932ddd11a6c2cd6a
Mouad Sodor <sodormouad@gmail.com>
2023-04-11 11:30:16 +0000

    mandatory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - calculate the length of @str
- * @str: string to work with
- * Return: the length
- */
-
-int _strlen(char *str)
-{
-	int i = 0;
-
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
  * _strdup - duplicates a striing
  * @str: the string to duplicate
  * Return: a pointer to the new allocated string
@@ -25,14 +8,17 @@ int _strlen(char *str)
 
 char *_strdup(char *str)
 {
-	int len, i = 0;
+	int len, i;
 	char *dup;
 
-	len = _strlen(str);
-	dup = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	dup = malloc(sizeof(char) * (len + 1));
 	if (!dup)
 		return (NULL);
-	while (str[i])
+	for (i = 0; str[i] != '\0'; i++)
 		dup[i] = str[i];
 	dup[i] = '\0';
 	return (dup);
