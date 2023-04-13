b07e74a613cf0eea27b6c0374bfc6e6e279b41ec
Mouad Sodor <sodormouad@gmail.com>
2023-04-13 21:24:58 +0000

    mandatory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,8 +19,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (len2 = 0; s2[len2]; len2++)
 		;
-	/*if (len2 < n)
-		str = malloc(len1 + len2 + 1);*/
 	str = malloc(len1 + n + 1);
 	if (!str)
 		return (NULL);
