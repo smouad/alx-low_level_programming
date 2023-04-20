99312fde810f4d27c1fb95c362f4771d75e777df
Mouad Sodor <sodormouad@gmail.com>
2023-04-20 01:20:45 +0000

    add README

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function pointer
+ * print_name - a function that prints a name
+ * @name: the name to print
+ * @f: a pointer to the function that gonna print @name
  * Return: void
  */
 
