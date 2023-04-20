e6cdc4f1a7a07714601de124782651b42b587def
Mouad Sodor <sodormouad@gmail.com>
2023-04-20 23:23:31 +0000

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (separator)
+		if (separator && i < (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
