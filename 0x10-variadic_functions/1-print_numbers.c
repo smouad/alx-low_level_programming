85217b8882e28a3aa5dfdc2f5c1e7c563fec0b07
Mouad Sodor <sodormouad@gmail.com>
2023-04-20 23:19:37 +0000

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the numbers passe to is separated by @separator
+ * @separator: separates the integers
+ * @n: number of integers
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (n == 0)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator)
+			printf("%s", separator);
+	}
+}
