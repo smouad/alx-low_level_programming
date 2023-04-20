ebc77bf6fd35d7e090ee5a025ec2e9e77d0ff54c
Mouad Sodor <sodormouad@gmail.com>
2023-04-20 02:37:33 +0000

    add README

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - do operatioon
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error: Incorect argumen number!\n");
+		exit(1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	result = op_func(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,72 @@
-#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-op_add : returns the sum of a and b.Prototype : int op_add(int a, int b);
-op_sub : returns the difference of a and b.Prototype : int op_sub(int a, int b);
-op_mul : returns the product of a and b.Prototype : int op_mul(int a, int b);
-op_div : returns the result of the division of a by b.Prototype : int op_div(int a, int b);
-op_mod : returns the remainder of the division of a by b.Prototype : int op_mod(int a, int b);
+/**
+ * op_add - adds @a and @b
+ * @a: first nbr
+ * @b: second nbr
+ * Return: returns the sum of @a and @b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract @a and @b
+ * @a: first nbr
+ * @b: second nbr
+ * Return: returns the diffrence of @a and @b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiples @a and @b
+ * @a: first nbr
+ * @b: second nbr
+ * Return: returns the product of @a and @b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide @a and @b
+ * @a: first nbr
+ * @b: second nbr
+ * Return: returns the division of @a and @b
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of @a and @b
+ * @a: first nbr
+ * @b: second nbr
+ * Return: returns the remainder of the division of @a and @b
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a % b);
+}
