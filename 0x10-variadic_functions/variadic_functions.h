00645ea76b000fe834a6f025712269fb90b0cc85
Mouad Sodor <sodormouad@gmail.com>
2023-04-21 10:49:17 +0000

    edit: variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,7 @@
  * @c: the format
  * @print_func: pointer to the function to print
  */
+
 typedef struct s_print
 {
 	char *c;
@@ -19,5 +20,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
+
+void print_c(va_list args);
+void print_i(va_list args);
+void print_f(va_list args);
+void print_s(va_list args);
 
 #endif
