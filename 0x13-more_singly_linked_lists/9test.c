420fd9efcfe25abb880f216bc4610eaa51edafd4
Mouad Sodor <sodormouad@gmail.com>
2023-05-03 00:13:46 +0100

    task 9

--- 0x13-more_singly_linked_lists/9test.c
+++ 0x13-more_singly_linked_lists/9test.c
@@ -5,7 +5,6 @@
  * @head: pointer to the address of first node
  * @idx: insert position
  * @n: data;
- * Return: null
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -36,4 +35,36 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp->next = new_node;
 
 	return (new_node);
+}
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    printf("-----------------\n");
+    insert_nodeint_at_index(&head, 5, 4096);
+    print_listint(head);
+    free_listint2(&head);
+    return (0);
 }
