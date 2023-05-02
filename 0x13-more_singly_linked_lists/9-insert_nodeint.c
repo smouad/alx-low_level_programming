e3a10730377cdf120288d61912ca43cc0ef5ac32
Mouad Sodor <sodormouad@gmail.com>
2023-05-02 21:33:28 +0100

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp, *new;
+	listint_t *new, *tmp;
 	unsigned int i = 0;
 
 	new = malloc(sizeof(listint_t));
