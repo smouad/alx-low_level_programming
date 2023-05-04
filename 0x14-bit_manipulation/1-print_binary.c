3e9e93c8d583d5b14736a78c98928c591c912438
Mouad Sodor <sodormouad@gmail.com>
2023-05-05 00:14:08 +0100

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_binary - print a binary of @n
+ * @n: to ptint
+*/
+void print_binary(unsigned long int n)
+{
+	int count = 31;
+
+	while ((n & 1 << count) == 0 && count > 0)
+		count--;
+
+	while (count >= 0)
+	{
+		if (n & 1 << count)
+			write(1, "1", 1);
+		else
+			write(1, "0", 1);
+		count--;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,6 @@
 #include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
 
 #endif
