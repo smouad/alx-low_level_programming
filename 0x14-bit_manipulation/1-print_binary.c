e85ba3e4e8a9ed42030115885965cc0aa870c4fe
Mouad Sodor <sodormouad@gmail.com>
2023-05-05 01:54:54 +0100

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,17 +6,7 @@
 */
 void print_binary(unsigned long int n)
 {
-	int count = (sizeof(unsigned long int) * 8) - 1;
-
-	while ((n & 1 << count) == 0 && count > 0)
-		count--;
-
-	while (count >= 0)
-	{
-		if (n & 1 << count)
-			write(1, "1", 1);
-		else
-			write(1, "0", 1);
-		count--;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
