f908f97de11798b4c83ff346ad1ba17bb5de584d
Mouad Sodor <sodormouad@gmail.com>
2023-05-05 01:25:18 +0100

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,9 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= 63)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	if (n << index & 1)
+	if (n & 1 << index)
 		return (1);
 	else
 		return (0);
