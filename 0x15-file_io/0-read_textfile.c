178c9115cceac9c88d0db373ebde7d25b01b5632
smouad <sodormouad@gmail.com>
2023-11-28 00:03:06 +0100

    update all

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,37 @@
 #include "main.h"
-
 /**
  * read_textfile - reads a text file and prints it to the standard output
  * @filename: name of the file to be read
  * @letters: number of letters to read and print
  * Return: the number of letters printed, or 0 if it failed
  */
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t read_count, write_count;
-    char *buffer;
-
-    if (filename == NULL)
-        return (0);
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
-
-    read_count = read(fd, buffer, letters);
-    if (read_count == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    write_count = write(STDOUT_FILENO, buffer, read_count);
-    if (write_count == -1 || write_count != read_count)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    free(buffer);
-    close(fd);
-
-    return (write_count);
+	int fd;
+	int i, y;
+	char *buf;
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
