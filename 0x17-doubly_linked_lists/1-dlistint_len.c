e98a528f31b2541a1e0516d9706df952586502b8
smouad <sodormouad@gmail.com>
2024-01-11 21:25:09 +0100

    list size

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements 
+ * in a linked dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of elements in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
