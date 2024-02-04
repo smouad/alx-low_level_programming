c50ca0bc40297d9ede67bc787aa268a5a4cdd6ac
smouad <sodormouad@gmail.com>
2024-02-04 13:25:54 +0100

    push

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: key to find index of
+ * @size: size of the array of the hash table
+ *
+ * Return: index at which the key/value pair should be stored in the array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = hash_djb2(key) % size;
+
+	return (index);
+}
